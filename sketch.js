75e68642d629a12f8bcdd7c266f62118148e7b04
riyamehrin <86054695+riyamehrin@users.noreply.github.com>
2021-11-16 15:50:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,34 +9,65 @@ const Composite = Matter.Composite;
 
 let engine;
 let world;
-var ground,fruit,rope,flink;
+var ground, bridge;
+var leftWall, rightWall;
+var jointPoint;
+var jointLink;
 
-function setup() 
-{
-  createCanvas(500,700);
-  frameRate(80);
+var stones = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(200,680,600,20);
-  rope=new Rope(7,{x:245,y:30});
-  fruit=Bodies.circle(300,300,20);
-  Matter.Composite.add(rope.body,fruit);
-  flink=new Link(rope,fruit)
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
-  textSize(50)
+  frameRate(80);
+
+  ground = new Base(0, height - 10, width * 2, 20, "#795548", true);
+  leftWall = new Base(300, height / 2 + 50, 600, 100, "#8d6e63", true);
+  rightWall = new Base(width - 300, height / 2 + 50, 600, 100, "#8d6e63", true);
+
+  /*bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
+  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
+
+  bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
+  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);
+
+  /*bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
+  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
+
+  /*bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
+  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
+
   
+  Matter.Composite.add(bridge.body, jointPoint);
+
+  //Matter.Composite.add(jointPoint);
+  
+  //Matter.Composite.add(jointPoint, bridge.body);
+  
+  //Matter.Composite.add(bridge.body);
+
+
+  jointLink = new Link(bridge, jointPoint);
+
+  for (var i = 0; i <= 8; i++) {
+    var x = random(width / 2 - 200, width / 2 + 300);
+    var y = random(-10, 140);
+    var stone = new Stone(x, y, 80, 80);
+    stones.push(stone);
+  }
 }
 
-function draw() 
-{
+function draw() {
   background(51);
-  ground.show();
-  
   Engine.update(engine);
-  rope.show();
-  ellipse(fruit.position.x,fruit.position.y,30,30);  
-  
- 
-   
+
+  ground.show();
+  bridge.show();
+  leftWall.show();
+  rightWall.show();
+
+  for (var stone of stones) {
+    stone.show();
+  }
 }
